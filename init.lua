6697c6f1fa5b9e69b37cae9fa940b1d22c722c68
sparktx-james-evans <James.Evans@sparktx.com>
2024-02-27 04:16:41 +0000

    Mv keymaps.

--- init.lua
+++ init.lua
@@ -624,23 +624,6 @@ cmp.setup {
   },
 }
 
--- save file 
-vim.api.nvim_set_keymap('n', '<Leader>fs', ':w<CR>', {noremap = true, silent = true})
---vim.cmd("nnoremap <Leader>k :w<CR>")
-vim.cmd("autocmd InsertLeave * :w")
-
--- window control
---
--- -- Set a normal mode, non-recursive keymap
--- split below
-vim.api.nvim_set_keymap('n', '<Leader>ws', '<C-w>s', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<Leader>wj', '<C-w>j', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<Leader>wk', '<C-w>k', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<Leader>wl', '<C-w>l', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<Leader>wh', '<C-w>h', {noremap = true, silent = true})
--- maximize
-vim.api.nvim_set_keymap('n', '<Leader>wm', '<C-w>m', {noremap = true, silent = true})
-
 -- vim.cmd("nnoremap <Leader>ws :<C-w>s")
 -- vim.cmd("nnoremap <Leader>wj :<C-w>j")
 -- vim.cmd("nnoremap <Leader>wk :<C-w>k")
