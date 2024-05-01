c04095f237c2920fa7d9bb62bc6ce83d172a2fe2
sparktx-james-evans <James.Evans@sparktx.com>
2024-05-01 13:08:51 +0000

    Ubuntu setup.

--- init.lua
+++ init.lua
@@ -708,6 +708,7 @@ require('lazy').setup({
           end, { 'i', 's' }),
         },
         sources = {
+          { name = "copilot", group_index=2 },
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
@@ -799,6 +800,7 @@ require('lazy').setup({
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
+
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,9 +6,16 @@
 --
 vim.opt.iskeyword:remove("_")
 
+-- sm_run
+--local open_and_modify = require'sm_run'
+-- Map a key combination to the open_and_insert function
+--vim.api.nvim_set_keymap('n', '<Leader>o', function() plugin_name.open_and_insert(), {noremap = true, silent = true})
+-- end
+
+
 vim.api.nvim_set_keymap('n', '<Leader>fs', ':w<CR>', {noremap = true, silent = true})
 --vim.cmd("nnoremap <Leader>k :w<CR>")
-vim.cmd("autocmd InsertLeave * :w")
+--vim.cmd("autocmd InsertLeave * :w")
 
 -- window control
 --
@@ -26,9 +33,59 @@ vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
 vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
 
 return {
+	{"samesense/nvim-sm-helpers",
+		branch="main",
+		config = function ()
+			require("plugin_name")
+			vim.keymap.set('n', '<Leader>o',
+				function() plugin_name.open_and_insert() end )
+		end
+	},
+
 	{
 		"nvim-neo-tree/neo-tree.nvim",
 		branch = "v3.x", dependencies = {"nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim",
-		}                                                                                                                                                  
+		}
 	},
+  {
+        'zbirenbaum/copilot.lua',
+		cmd = "Copilot",
+        event = 'InsertEnter',
+		config = function()
+    require("copilot").setup({})
+  end,
+    },
+
+-- 	{
+--     "ThePrimeagen/harpoon",
+--     branch = "harpoon2",
+--     dependencies = { "nvim-lua/plenary.nvim" },
+--     config = function ()
+-- 	local harpoon = require("harpoon")
+-- 	harpoon:setup()
+-- 	vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+-- 	vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+-- 	vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+--     end
+-- },
+
+	{
+  "zbirenbaum/copilot-cmp",
+  config = function ()
+    require("copilot_cmp").setup()
+  end
+},
+
+
+  -- {
+  --       'zbirenbaum/copilot.lua',
+  --       dependencies = {
+  --           'zbirenbaum/copilot-cmp',
+  --       },
+  --       event = 'InsertEnter',
+  --       config = function()
+  --           require('plugin-config.copilot_config')
+  --       end,
+  --   },
+
 }
