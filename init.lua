4ada7f926544785f82f9326cd2d2d2ffb9ee5f77
sparktx-james-evans <James.Evans@sparktx.com>
2024-07-23 01:47:14 +0000

    add snakemake plugin

--- init.lua
+++ init.lua
@@ -739,6 +739,14 @@ require('lazy').setup({
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
+  -- local plugins need to be explicitly configured with dir
+  -- { dir = "~/tools/nvim-plugins/trash.nvim",
+  -- config = function ()
+  --                 require("trash")
+  --                 vim.keymap.set('n', '<Leader>o',
+  --                     function() require("trash").open_and_insert() end )                                                                                                               
+  --               end},
+
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -33,14 +33,14 @@ vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
 vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
 
 return {
-	{"samesense/nvim-sm-helpers",
-		branch="main",
-		config = function ()
-			require("plugin_name")
-			vim.keymap.set('n', '<Leader>o',
-				function() plugin_name.open_and_insert() end )
-		end
-	},
+	-- {"samesense/nvim-sm-helpers",
+	-- 	branch="main",
+	-- 	config = function ()
+	-- 		require("plugin_name")
+	-- 		vim.keymap.set('n', '<Leader>o',
+	-- 			function() plugin_name.open_and_insert() end )
+	-- 	end
+	-- },
 
 	{
 		"nvim-neo-tree/neo-tree.nvim",
@@ -70,11 +70,79 @@ return {
 -- },
 
 	{
+  "samesense/snakemake.nvim",
+config = function () require("snakemake") vim.keymap.set('n', '<Leader>o', function() require("snakemake").open_and_insert() end ) end},
+
+	{
   "zbirenbaum/copilot-cmp",
   config = function ()
     require("copilot_cmp").setup()
   end
 },
+
+{
+   "m4xshen/hardtime.nvim",
+   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+   opts = {}
+},
+
+	{
+    "svampkorg/moody.nvim",
+    event = { "ModeChanged", "BufWinEnter", "WinEnter" },
+    dependencies = {
+        -- or whatever "colorscheme" you use to setup your HL groups :)
+        -- Colours can also be set within setup, in which case this is redundant.
+        "catppuccin/nvim",
+    },
+    opts = {
+        -- you can set different blend values for your different modes.
+        -- Some colours might look better more dark, so set a higher value
+        -- will result in a darker shade.
+        blends = {
+            normal = 0.2,
+            insert = 0.2,
+            visual = 0.25,
+            command = 0.2,
+            operator = 0.2,
+            replace = 0.2,
+            select = 0.2,
+            terminal = 0.2,
+            terminal_n = 0.2,
+        },
+        -- there are two ways to define colours for the different modes.
+        -- one way is to define theme here in colors. Another way is to
+        -- set them up with highlight groups. Any highlight group set takes
+        -- precedence over any colours defined here.
+        colors = {
+            normal = "#00BFFF",
+            insert = "#70CF67",
+            visual = "#AD6FF7",
+            command = "#EB788B",
+            operator = "#FF8F40",
+            replace = "#E66767",
+            select = "#AD6FF7",
+            terminal = "#4CD4BD",
+            terminal_n = "#00BBCC",
+        },
+        -- disable filetypes here. Add for example "TelescopePrompt" to
+        -- not have any coloured cursorline for the telescope prompt.
+        disabled_filetypes = { "TelescopePrompt" },
+        -- you can turn on or off bold characters for the line numbers
+        bold_nr = true,
+        -- you can turn on and off a feature which shows a little icon and
+        -- registry number at the end of the CursorLine, for when you are
+        -- recording a macro! Default is false.
+        recording = {
+            enabled = false,
+            icon = "󰑋",
+            -- you can set some text to surround the recording registry char with
+            -- or just set one to empty to maybe have just one letter, an arrow
+            -- perhaps! For example recording to q, you could have! "󰑋    q" :D
+            pre_registry_text = "[",
+            post_registry_text = "]",
+        },
+    },
+  },
 
 
   -- {
